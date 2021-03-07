43f187cb187d0d2e8104753ae790a755153c5631
Sean Kramer <seandominickramer@gmail.com>
2021-03-06 19:11:28 -0500

    Add post endpoint to task router

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,16 @@
 // build your `/api/tasks` router here
+
+const tasksModel = require('./model')
+const router = require('express').Router()
+
+router.post('/', async (req, res, next) => {
+  try {
+const newTask = await tasksModel.createTask(req.body)
+
+res.status(201).json(newTask)
+  } catch(err) {
+    next(err)
+  }
+})
+
+module.exports = router
