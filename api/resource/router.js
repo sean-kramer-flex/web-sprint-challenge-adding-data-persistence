7e6627af6eff865e6c50ada0a2a2e8222a21f1a1
Sean Kramer <seandominickramer@gmail.com>
2021-03-06 19:06:55 -0500

    Add resource router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,25 @@
-// build your `/api/resources` router here
+const resourcesModel = require('./model')
+const router = require('express').Router()
+
+router.post('/', async (req, res, next) => {
+  try {
+const newResource = await resourcesModel.createResource(req.body)
+
+res.status(201).json(newResource)
+  } catch(err) {
+    next(err)
+  }
+})
+
+
+router.get('/', async (req, res, next) => {
+  try {
+const data = await resourcesModel.getResources()
+
+res.json(data)
+  } catch(err) {
+    next(err)
+  }
+})
+
+module.exports = router
