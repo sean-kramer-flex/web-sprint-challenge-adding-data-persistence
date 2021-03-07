07fa5e8a4519656b49cac87771e883dd1ff7061c
Sean Kramer <seandominickramer@gmail.com>
2021-03-06 20:15:42 -0500

    Change get task endpoint to include project data in return

--- api/task/model.js
+++ api/task/model.js
@@ -6,7 +6,7 @@ function createTask (task) {
 }
 
 function getTasks () {
-  return db('tasks')
+  return db('tasks as t').innerJoin('projects as p', 't.project_id', 'p.project_id').select('t.*', 'p.project_name', 'p.project_description')
 }
 
 
